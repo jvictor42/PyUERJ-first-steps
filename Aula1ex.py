1c022ab9aaf261ee6234733e10fc11e79ec5d668
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-06 08:18:02 -0300

    Create Aula1ex.py

---
+++ Aula1ex.py
@@ -0,0 +1,15 @@
+#Aula 1 exemplos
+#Texto (Zen do Python)
+import this
+#Texto (Tirinha do Python)
+import antigravity
+#exercício 1 (escrever na tela)
+print('mundo, mundo, vasto mundo')
+#exercício 3 (Usar como calculadora)
+4+2
+4-2
+4/2
+4*2
+#exercício 2 (comando help do print)
+help('print')
+#-----------------------------------------------------
