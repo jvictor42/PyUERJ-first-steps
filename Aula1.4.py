1249d05e1546fd99af689603d52e3afaebe05c88
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-06 00:40:02 -0300

    Create Aula1.4.py

---
+++ Aula1.4.py
@@ -0,0 +1,11 @@
+#Exercício 4 Aula 1
+#Se, ao meiodia, a sombra de um poste de 5 m de altura tem apenas 50 cm de comprimento no chão, qual o ângulo zenital do sol? 
+c_a=5
+c_o=0.5
+pi=3.14159265359
+tan=c_o/c_a
+theta=(180*tan)/pi
+#Apresentação dos resultados
+print('Se, ao meiodia, a sombra de um poste de 5 m de altura tem apenas 50 cm de comprimento no chão, qual o ângulo zenital do sol?')
+print('ângulo zenital=',theta)
+print('Fim!')
