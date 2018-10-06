c870f389a8172f4a48759d2c4a86570237157609
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-06 14:06:22 -0300

    Create Aula2.1.py

---
+++ Aula2.1.py
@@ -0,0 +1,21 @@
+# Exercício 1 Aula 2
+#Para cada uma das expressões a seguir, escreva o valor das expressões e o tipo (do valor da expressão):
+largura=17
+altura=12.0
+delimitador="."
+a=largura/2
+b=largura/(2.0)
+c=altura*3
+d=1+2*5
+#Resultados obtidos
+print('Para cada uma das expressões a seguir, escreva o valor das expressões e o tipo (do valor da expressão):')
+print('largura/2=',a)
+type(a)
+print('largura/(2.0)=',b)
+type(b)
+print('altura*3=',c)
+type(c)
+print('1+2*5=',d)
+type(d)
+print('Fim!')
+#O programa rodou, mas o type() nao apareceu.
