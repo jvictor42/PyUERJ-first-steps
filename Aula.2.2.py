b2c90e5d45954d0bb480adda270e0a48123dca55
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-06 14:45:00 -0300

    Create Aula.2.2.py

---
+++ Aula.2.2.py
@@ -0,0 +1,14 @@
+# Exercício 2 Aula 2
+#Qual é o volume de uma esfera de raio 5?
+r=5
+c=(4*3.1415)/3
+V1=c*(r**3)
+print('Qual é o volume de uma esfera de raio 5?')
+print('v=',V1)
+#Volume pra um R dado pelo usuário
+r1=input('Agora digite um valor diferente para o raio: ')
+float(r1)
+print()
+V2=c*(r1**3)
+print('O volume é ',V2)
+print('Fim!')
