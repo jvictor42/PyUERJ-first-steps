7a2cb2cde75613f680ea757a49c8ce0a5e1034a6
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-10 00:08:36 -0300

    Update Aula3.3.py

--- Aula3.3.py
+++ Aula3.3.py
@@ -1,6 +1,7 @@
 # Exercício 3 Aula 3
 #Crie uma funcão para calcular o ángulo zenital do sol (da semana passada) tomando como argumento as medidas da altura e o comprimento da sombra.
 print('Ângulo zenital do sol cuja sombra de um poste de 5m, tem 50cm.')
+import math
 def ang_zen(x,y):
     teta=math.degrees(math.atan(x/y))
     print(teta)
