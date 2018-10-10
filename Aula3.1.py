4bdb87397b62c75fe2e54967019c1bd1817d9c41
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-10 06:48:49 -0300

    Create Aula3.1.py

---
+++ Aula3.1.py
@@ -0,0 +1,8 @@
+# Exercício 1 Aula 3
+#Crie uma funcão que tome um argumento e imprima o valor e o tipo dele.
+def funcao(x,y):
+    eq=9*(x**2)/y
+    print(eq)
+
+print(eq(3,2),' resultado de uma equação do segundo grau.')
+print('Fim!')
