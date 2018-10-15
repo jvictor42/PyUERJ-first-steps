1ef04a1faba9533b5922c77b5da6560920969781
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-15 13:58:43 -0300

    Update Aula4_ex.py

--- Aula4_ex.py
+++ Aula4_ex.py
@@ -1,20 +1,50 @@
-#Aula 4 exemplos
-#exemplo 1--------------------------------
-#exemplo ultimo-----------------------------
-def do_twice(f):
-    f ()
-    f ()
-def print_spaml (): 
-    print('spam') 
+def meuverso(palavras): #definiu a função com parametro	
+    print(palavras)
+    print(palavras)     #Escreveu o que precisa ser feito, no caso print 2 vezes do parametro
+    
+def minhacancao(verso1, verso2): #cria uma nova função com 2 parametros agora
+    meuverso(verso1)
+    meuverso(verso2)  #define as instruções, porem a instrução é a função dita antes.
+    
+minhacancao("lalarí","lalará")  #chama a segunda função mas definindo valores pros parametros.
 
-do_twice (print_spaml)
-#exemplo ultimo (modificado)-----------------------------
-#Modifique do_twice para que sejam necessários dois argumentos, um objeto de função e um valor, e chame a função duas vezes, passando o valor como um argumento.
-def do_twice(f,x):
-    f (x)
-    f (x)
-def print_spam (): #Não da pra usar isso pq precisa de um argumento
-    print('spam') 
+#REPARE: da pra criar funções com o mesmo parametro, pq uma vez executada a função, ela "desaparece".
+def meuverso(palavras):  
+    print(palavras)
+    print(palavras)
+    
+def minhacancao(verso1, verso2):
+    meuverso(verso1)
+    meuverso(verso2)
+    
+minhacancao("naaaaa na na nana nanaa","tudum tudum dum")
 
-do_twice (print_spam)
-#Escreva uma versão mais geral de print_spam, chamada print_twice, que use uma string como parâmetro e imprima duas vezes.
+#POREM eu poderia só chamar a função, mudando o argumento dos parametros.
+minhacancao("ha " *3, "ho "*3)
+
+#Agora eu to definindo uma variavel c, depois chamo ela como parametro da função meuverso.
+c = "I want to ride my bicycle"
+meuverso(c)
+
+#agora definindo outra função TENTANDO usar a primeira função dentro.
+def gato_duplo (parte1, parte2): 
+    gato = parte1 + parte2 
+    #a=variavel_qualquer
+    meuverso (gato) #REPARE QUE essa linha de comando não executa a primeira função. Isso pq quando cria uma variavel dentro da função, ela é local e existe apenas dentro da função. Porem estão associadas agora.
+
+#print(a)--> essa linha de comando daria erro no codigo, pq mesmo que definida dentro da função, ela só existe pra auxiliar na execução da mesma.
+
+#Agora se definirmos duas variaveis, podemos associa-las aos parametros de uma função existente.
+linha1 = "blim "
+linha2 = "blem! "
+gato_duplo(linha1,linha2) #linha1=parte1 e linha2=parte2
+
+#print(gato)--> Se tentarmos imprimir a variavel gato, da erro. Após a execusão da função a ariavel é destruida.
+linha1= "miau"
+linha2= "grrr miau"
+gato_duplo(linha1,linha2)
+#Se atribuir à uma variavel o resultado de uma função vazia, pode receber um valor "None"
+resultado = meuverso("under pressure")
+print(resultado)
+print(type(resultado))
+print('Fim!')
