8a76f71f72f4a1d7125ad83150c326c739af7d6c
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-17 17:55:53 -0300

    Update Aula4.1.py

--- Aula4.1.py
+++ Aula4.1.py
@@ -1,48 +1,16 @@
+#Aula 4 exercicio 1 site
+#Crie uma função que imprima na tela a data de hoje em português (Utilize a libraria time e a função localtime, que tipo de objeto ela retorna?
 import time
-t= time.localtime()
-print(t) # este e o objeto que contem o retorno da chamada anterior
-
-
-
-def dia_semana(w):
-    nome_dia=["2a-feira",
-              "3a-feira",
-              "4a-feira",
-              "5a-feira",
-              "6a-feira",
-              "Sabado",
-              "Dmingo"]
-    return nome_dia[w]
-
-def mes(m):
-    nome_mes=["Janeiro",
-              "Fevereiro",
-              "Marco",
-              "Abril"
-              "Maio",
-              "Junho",
-              "Julho",
-              "Agosto",
-              "Setembro",
-              "Outubro",
-              "Novembro",
-              "Dezembro" ]
-    return nome_mes[m-1]
-
-def mostrar_dia_hora():
-    lt=time.localtime()
-    print("\n\n**********************************************************************\n")
-    print("Hoje é ",dia_semana(lt.tm_wday),
-          " ",
-          lt.tm_mday,
-          " de ",mes(lt.tm_mon),
-          " de ",lt.tm_year,
-          " e são as ", lt.tm_hour,
-          ":",lt.tm_min,
-          "horas\n")
-    print("**********************************************************************\n")
-    #    gmt=time.gmtime()
-
-
-    
-mostrar_dia_hora()
+a=time.localtime()
+ano=a.tm_year
+mes=a.tm_mon
+dia=a.tm_mday
+hora=a.tm_hour
+min=a.tm_min
+sem=a.tm_wday
+def semana(x):
+    f_sem=['segunda-feira', 'terça-feira', 'quarta-feira','quinta-feira', 'sexta-feira']
+    return f_sem[x]
+print('Hora:',hora,':',min,
+       'Data:',dia,'/',mes,'/',ano, semana(sem))
+print('Fim!')
