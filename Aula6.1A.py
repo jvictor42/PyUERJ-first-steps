393841c0560886e30634ede1ac96bc3f22467344
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-19 14:51:40 -0300

    Create Aula6.1A.py

---
+++ Aula6.1A.py
@@ -0,0 +1,10 @@
+#Aula 6  exercicio 1 da apostila
+#Faça modificações similares para permitir que o usuário mude a cor da tela antes
+c=input('Insara a cor da janela: ')
+import turtle
+jn=turtle.Screen()
+jn.bgcolor(c)
+rubinho=turtle.Turtle()
+rubinho.pensize(4)
+
+
