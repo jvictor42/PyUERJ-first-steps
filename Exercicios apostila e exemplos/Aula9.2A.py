d32abc2ee72e9c94aa409ae3dd9935eba2536d34
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-10-31 18:01:10 -0300

    Create Aula9.2A.py

---
+++ Exercicios apostila e exemplos/Aula9.2A.py
@@ -0,0 +1,29 @@
+#Aula 9 Exercicio 2 apostila
+#Queremos obter uma tabela ou gráfico da distância percorrida en função do tempo e o tempo total para chegar em 7 km, faça isto utilizando listas.
+print('Lista de distâncias percorridas em 12km/h a cada 1min')
+cam=5
+vel=12
+def min_hora(min):
+    h=min/60
+    return(h)
+    
+def dist_cons(t,v):
+    dmin=v*t
+    return(dmin)
+
+dacel=200   
+def acel()
+    a=(15-vel)/t1
+    
+def dist_cons15(t,v):
+    dmin15=v*t
+    return(dmin15)
+
+
+dist_list=[]
+dm=0
+while dm <= 5:
+    dm=dist_cons(min_hora(1),vel) + dm
+    dist_list.append(dm)
+print(dist_list)
+print('fim')
