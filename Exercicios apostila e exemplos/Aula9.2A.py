3c6b5e27ec23f229a8bc5bd8ba294326b0da6d56
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-11-03 12:38:37 -0300

    Update Aula9.2A.py

--- Exercicios apostila e exemplos/Aula9.2A.py
+++ Exercicios apostila e exemplos/Aula9.2A.py
@@ -1,29 +1,36 @@
-#Aula 9 Exercicio 2 apostila
-#Queremos obter uma tabela ou gráfico da distância percorrida en função do tempo e o tempo total para chegar em 7 km, faça isto utilizando listas.
-print('Lista de distâncias percorridas em 12km/h a cada 1min')
-cam=5
-vel=12
+# Aula 9 Exercicio 2 apostila
+# Queremos obter uma tabela ou gráfico da distância percorrida en função do tempo e o tempo total para chegar em 7 km, faça isto utilizando listas.
+print('Lista de distâncias percorridas por uma corredora a cada 1min')
+cam = 5
+vel = 12
+
 def min_hora(min):
-    h=min/60
-    return(h)
-    
-def dist_cons(t,v):
-    dmin=v*t
-    return(dmin)
+    h = min / 60
+    return (h)
 
-dacel=200   
-def acel()
-    a=(15-vel)/t1
-    
-def dist_cons15(t,v):
-    dmin15=v*t
-    return(dmin15)
+def dist_cons(t, v):
+    dmin = v * t
+    return (dmin)
+
+dist2 = 0.2
+vel2 = 15
+def dist_var(t,v0,v):
+    dvar = (t/2)*(v-v0)
+    return (dvar)
 
 
-dist_list=[]
-dm=0
-while dm <= 5:
-    dm=dist_cons(min_hora(1),vel) + dm
-    dist_list.append(dm)
+dist_list = []
+dm = 0
+while dm <= 7:
+    if dm <=5:
+        dm = dist_cons(min_hora(1), vel) + dm
+        dist_list.append(dm)
+    elif dm >5 and <=5.2:
+        dm = dist_var(min_hora(1),vel,vel2)
+        dist_list.append(dm)
+    else:
+        dm = dist_cons(min_hora(1), vel) + dm
+        dist_list.append(dm)
+        
 print(dist_list)
 print('fim')
