958ce0fad49bf30ec803d6cbc67967cfc0f892d0
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-11-07 07:56:13 -0200

    Update Aula9.2A.py

--- Exercicios apostila e exemplos/Aula9.2A.py
+++ Exercicios apostila e exemplos/Aula9.2A.py
@@ -4,20 +4,19 @@ print('Lista de distâncias percorridas por uma corredora a cada 1min')
 cam = 5
 vel = 12
 
-def min_hora(min):
-    h = min / 60
-    return (h)
+def min_hora(minu):
+    h = minu / 60
+    return(h)
 
 def dist_cons(t, v):
     dmin = v * t
-    return (dmin)
+    return(dmin)
 
 dist2 = 0.2
 vel2 = 15
 def dist_var(t,v0,v):
     dvar = (t/2)*(v-v0)
-    return (dvar)
-
+    return(dvar)
 
 dist_list = []
 dm = 0
@@ -25,12 +24,23 @@ while dm <= 7:
     if dm <=5:
         dm = dist_cons(min_hora(1), vel) + dm
         dist_list.append(dm)
-    elif dm >5 and <=5.2:
+        #print('1')
+    if dm > 5 and dm < 5.2:
         dm = dist_var(min_hora(1),vel,vel2)
         dist_list.append(dm)
-    else:
+        #print('2')
+    if dm >= 5.2:
         dm = dist_cons(min_hora(1), vel2) + dm
         dist_list.append(dm)
-        
+        print('3')
 print(dist_list)
 print('fim')
+
+'''import matplotlib.pyplot as plt
+
+plt.plot(xxxxxxx,dist_list)
+plt.xlabel("tempo min")
+plt.ylabel("distância km")
+plt.show()
+plt.close()'''
+
