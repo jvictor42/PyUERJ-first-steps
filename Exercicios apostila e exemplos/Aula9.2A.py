6094068cdf4efe2312b642ea3e54c595a12f24c6
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-11-07 15:38:18 -0200

    Update Aula9.2A.py

--- Exercicios apostila e exemplos/Aula9.2A.py
+++ Exercicios apostila e exemplos/Aula9.2A.py
@@ -20,27 +20,39 @@ def dist_var(t,v0,v):
 
 dist_list = []
 dm = 0
+dist_list.append(0)
+
 while dm <= 7:
-    if dm <=5:
-        dm = dist_cons(min_hora(1), vel) + dm
-        dist_list.append(dm)
-        #print('1')
-    if dm > 5 and dm < 5.2:
-        dm = dist_var(min_hora(1),vel,vel2)
-        dist_list.append(dm)
-        #print('2')
-    if dm >= 5.2:
-        dm = dist_cons(min_hora(1), vel2) + dm
-        dist_list.append(dm)
-        print('3')
-print(dist_list)
-print('fim')
+	if dm <=5:
+        	dm = dist_cons(min_hora(1), vel) + dm
+        	dist_list.append(dm)
 
-'''import matplotlib.pyplot as plt
+	elif dm > 5 and dm < 5.2:
+		dm = dist_var(min_hora(1),vel,vel2) + dm
+		dist_list.append(dm)
 
-plt.plot(xxxxxxx,dist_list)
+	else:
+		dm = dist_cons(min_hora(1), vel2) + dm
+		dist_list.append(dm)
+
+time_list = []
+t = len(dist_list)
+tm = 0
+for i in range(t):
+    tm = tm + 1
+    time_list.append(tm)
+
+s = len(dist_list)
+for i in range(s):
+    print("%.2f" % dist_list[i]) #"%.2f" % reduz para duas casas decimais de incerteza.
+
+import matplotlib.pyplot as plt
+
+plt.plot(time_list,dist_list)
 plt.xlabel("tempo min")
 plt.ylabel("distância km")
 plt.show()
-plt.close()'''
+plt.close()
+
+print('Fim!')
 
