b5a246efb5556fc8d31938254dba6b9bedc74929
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-11-11 11:16:59 -0200

    Update projeto1.py

--- projetos/projeto1.py
+++ projetos/projeto1.py
@@ -24,46 +24,70 @@ def mod_raio(x_0,y_0):
     r_mod = math.sqrt(x_0**2 + y_0**2)
     return r_mod
 
-#Funções pẽndulo de mola. Os parametros x_0 e v_0 não representam os iniciais do sistema, mas os anteriores na contagem. 
-def aceleracaoX(k,m,r_mod,xn,l0,v_mod):
-    an_x = -(k/m)(1-(l0/r_mod)+(v_mod/r_mod)**2)*xn
-    return an_x
-def velocidadeX(var_t,t0,n,v_0,an_x):
-    vn_x = v_0 +an_x*var_t
+#Funções pẽndulo de mola. Os parametros x_0 e v_0 não representam os iniciais do sistema nem o eixo, mas os anteriores na contagem. 
+def aceleracao(k,m,r_mod,xn,l0,v_mod):
+    an = -(k/m)(1-(l0/r_mod)+(v_mod/r_mod)**2)*xn
+    return an
+def velocidade(var_t,v_0,an):
+    vn = v_0 +an*var_t
     return vn_x
-def posicaoX(var_t,x_0,vn_x):
-    xn = x_0+vn_x+var_t
+def posicao(var_t,x_0,vn):
+    xn = x_0+vn+var_t
     return xn
 
-#Definindo as listas vazias
+#Definindo as listas 
 list_z = []
 list_x = []
 list_velz = []
 list_velx = []
 list_mod_vel = []
 list_time = []
-list_r_mod = []
-list_a_mod = []
+list_mod_raio = []
+list_mod_acel = []
+
+#Função adiciona lista
+def list_ad(l1,l2,l3,l4,l5,l6,l7,l8):
+    list_z.append(l1)
+    list_x.append(l2)
+    list_mod_raio.append(l3)
+    list_velz.append(l4)
+    list_velx.append(l5)
+    list_mod_vel.append(l6)
+    list_time.append(l7)
+    list_mod_acel.append(l8)
 
 #Usuário
 print('Entre com os parametros do problema.')
-mass = float(input(' Massa do objeto: '))
-kons = float(input(' Constante elastica da mola: '))
-lnat = float(input(' Comprimento natural da mola: '))
-print('Quais coordenadas de posição inicial do pêndulo?')
+mass = float(input(' Massa do objeto em kg: '))
+kons = float(input(' Constante elastica da mola em N/m: '))
+lnat = float(input(' Comprimento natural da mola em m: '))
+print('Quais coordenadas de posição inicial do pêndulo em m?')
 x_ini = float(input('Em x: '))
-list_x.append(x_ini)
 z_ini = float(input('Em z: '))
-list_z.append(z_ini)
-print('Qual intervalo de tempo de oscilação?')
-t_ini = float(input(' tempo inicial: '))
-list_time.append(t_ini)
-t_fin = float(input(' tempo final: '))
+print('Qual intervalo de tempo de oscilação em segundos?')
+####t_ini = float(input(' tempo inicial: '))
+t_fin = int(input(' tempo final: '))
 
 #Estrutura sequencial
-a_cont=0
-v_cont=0
-r_cont=0
+ax=0
+az=0
+t_cont=0
+velx=0
+velz=0
+
+for i in range(0,t_fin):
+	if t_cont <=t_fin:
+	    m_raio = mod_raio(x_ini,z_ini) #modulo do raio
+	    m_vel = mod_vel(velx,velz)     #modulo da velocidade
+	    m_acel = mod_acel(ax,az) #modulo da aceleração
+	    list_ad(z_ini,x_ini,m_raio,velz,velx,m_vel,t_cont,m_acel) #adiciona nas listas
+	    ax = aceleracao(kons,mass,m_raio,x_ini,lnat,m_vel) #acel. em x
+	    az =-g-aceleracao(kons,mass,m_raio,z_ini,lnat,m_vel) #acel. em z
+	    velx = velx + velocidade(dt,velx,ax)
+	    velz = velz + velocidade(dt,velz,az)
+	    x_ini = x_ini + posicao(dt,x_ini,velx)
+	    z_ini = z_ini + posicao(dt,z_ini,velz)
+	    t_cont = t_cont + i*dt
 
 cond='s'
 condu = input('Deseja ir para o pêndulo simples? s/n')
