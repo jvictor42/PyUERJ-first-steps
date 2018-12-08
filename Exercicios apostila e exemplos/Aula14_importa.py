6e717609a5697fa5bceca095b1e9e442ec717d93
José Victor <43897428+jvictor42@users.noreply.github.com>
2018-12-08 02:50:27 -0200

    Update Aula14_importa.py

--- Exercicios apostila e exemplos/Aula14_importa.py
+++ Exercicios apostila e exemplos/Aula14_importa.py
@@ -1,5 +1,17 @@
-#Aula 14 exercicio 3
+#Aula 14 exercicio 4
 #importa o modulo.
 import Aula14_mod as al
+import matplotlib.pyplot as plt
+import numpy as np
+
 nome = input('Digite o nome do arquivo|tabela. ')
-print(al.read_files(nome))
+par = input('Tipo de espaçamento. ')
+dicio = al.read_files(nome,par)
+print(dicio)
+
+n_bins = 13  #largura da coluna
+fig, axs = plt.subplots(1,len(dicio), sharey=True, tight_layout=True)
+for (n,valor) in enumerate(dicio):
+	#plt.xlabel(valor)
+	axs[n].hist(dicio[valor], bins=n_bins)
+plt.show()
